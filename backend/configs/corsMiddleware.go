ba3856644b54e473a537c5de07dfe663de78db00
co <119276763+ricotandrio@users.noreply.github.com>
2024-03-22 15:18:26 +0700

    fix: react cors middleware issue (#6)

---
+++ backend/configs/corsMiddleware.go
@@ -0,0 +1,15 @@
+package configs
+
+// CORSMiddleware adds CORS headers to responses
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+			c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+			c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+			if c.Request.Method == "OPTIONS" {
+					c.AbortWithStatus(http.StatusOK)
+					return
+			}
+			c.Next()
+	}
+}
