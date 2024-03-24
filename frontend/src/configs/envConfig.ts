8efc27896cf190dd2017f8b911d29e8f650ae15f
ricotandrio@gmail.com <ricotandrio@gmail.com>
2024-03-24 13:52:07 +0700

    fix: env config

--- frontend/src/configs/envConfig.ts
+++ frontend/src/configs/envConfig.ts
@@ -1,6 +1,7 @@
 // userService
-
 export const userServiceApi = {
-  login: import.meta.env.VITE_API_USER + '/login',
-  register: import.meta.env.VITE_API_USER + '/register',
+  login     : import.meta.env.VITE_API_LOGIN,
+  register  : import.meta.env.VITE_API_REGISTER,
+  logout    : import.meta.env.VITE_API_LOGOUT,
 };
+
